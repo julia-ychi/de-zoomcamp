46afbd8e078f7fccdf3f65ff747311bc7ccb0d42
Yuyuan Ji <jiyuyuan@umich.edu>
2024-02-05 16:27:55 +0000

    homework2 finished

---
+++ mage-zoomcamp/magic-zoomcamp/transformers/transform_data.py
@@ -0,0 +1,39 @@
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    data = data[(data['passenger_count'] > 0) & (data['trip_distance'] > 0)]
+    data['lpep_pickup_date'] = data['lpep_pickup_datetime'].dt.date
+    data.columns = (data.columns
+                .str.replace('(?<=[a-z])(?=[A-Z])', '_', regex=True)
+                .str.lower()
+             )
+    print(data['vendor_id'].unique())
+    return data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert (~output['vendor_id'].isin([1,2])).sum() == 0, 'Vendor ID is one of the existing values in the column'
+    assert output['passenger_count'].isin([0]).sum() == 0, 'There are rides with zero passengers'
+    assert output['trip_distance'].isin([0]).sum() == 0, 'There are rides with zero trip distance'
